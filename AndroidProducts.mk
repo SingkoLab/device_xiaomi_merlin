364cf8a73b333c8e9ff93c73374418468a5e4018
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-08-21 12:49:42 +0800

    merlin: Initial Device Tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_merlin.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_merlin-user \
+    lineage_merlin-userdebug \
+    lineage_merlin-eng
---
+++ README.md
@@ -0,0 +1,1 @@
+# Common device tree for Xiaomi merlin devices
