6a44f5966c75e15efe364d5fba254b52c74aed45
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-08-21 17:06:35 +0800

    merlin: Add firmware to Device makefiles

--- device.mk
+++ device.mk
@@ -136,6 +136,15 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Firmware
+RECOVERY_TS_FW_PATH := vendor/xiaomi/merlin/proprietary/vendor/firmware
+
+PRODUCT_COPY_FILES += \
+    $(RECOVERY_TS_FW_PATH)/novatek_ts_g6_fw.bin:recovery/root/vendor/firmware/novatek_ts_g6_fw.bin \
+    $(RECOVERY_TS_FW_PATH)/novatek_ts_g6_mp.bin:recovery/root/vendor/firmware/novatek_ts_g6_mp.bin \
+    $(RECOVERY_TS_FW_PATH)/novatek_ts_g6_mp.bin:recovery/root/vendor/firmware/novatek_ts_g6_mp.bin \
+    $(RECOVERY_TS_FW_PATH)/novatek_ts_72d_fw.bin:recovery/root/vendor/firmware/novatek_ts_72d_fw.bin \
+    $(RECOVERY_TS_FW_PATH)/novatek_ts_72d_mp.bin:recovery/root/vendor/firmware/novatek_ts_72d_mp.bin
+    
 COMMON_RECOVERY_TS_FW_PATH := vendor/xiaomi/mt6768-common/proprietary/vendor/firmware
 
 PRODUCT_COPY_FILES += \
