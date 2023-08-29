ec4e6fcd48c7e7a37a6df70b5a5ee8359763ee45
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-08-29 21:26:15 +0800

    merlin: Build Viper4Android

--- device.mk
+++ device.mk
@@ -60,6 +60,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Viper4Android
+$(call inherit-product, packages/apps/ViperFX/config.mk)
+
 # Biometrics
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.merlin
