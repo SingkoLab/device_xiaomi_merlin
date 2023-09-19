ed4f9ceeffdca805f44ea17cca4b2f807fe0ef07
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-09-19 14:40:51 +0800

    merlin: Change fingerprint to 12.5.7.0.RJOTWXM

--- lineage_merlin.mk
+++ lineage_merlin.mk
@@ -17,6 +17,6 @@ PRODUCT_MODEL := Redmi Note 9
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="merlin-user 11 RP1A.200720.011 V12.5.4.0.RJOMIXM release-keys"
+    PRIVATE_BUILD_DESC="merlin-user 11 RP1A.200720.011 V12.5.7.0.RJOTWXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.4.0.RJOMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.7.0.RJOTWXM:user/release-keys
