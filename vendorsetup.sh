cd80f1567ac4d5134a4a642d42d8afc648d25d02
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-10-31 15:41:40 +0800

    merlin: Clone dependencies using vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Clone dependencies
+git clone --depth 1 https://github.com/singko-prjkt/vendor_xiaomi_merlin -b 14 vendor/xiaomi/merlin
+
+git clone --depth 1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/mt6768
+
+git clone --depth 1 https://github.com/singko-prjkt/device_mediatek_sepolicy_vndr -b 14 device/mediatek/sepolicy_vndr
+
+git clone --depth 1 https://github.com/singko-prjkt/hardware_mediatek -b 14 hardware/mediatek
