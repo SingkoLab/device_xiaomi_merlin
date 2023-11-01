ba04b782aacdea9205f15e6c07fef5124376df03
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-11-01 10:00:31 +0800

    merlin: vendorsetup: Set Build User & Host

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
+export KBUILD_BUILD_USER=Subreker
+export KBUILD_BUILD_HOST=singko-prjkt
+export BUILD_USERNAME=Subreker
+export BUILD_HOSTNAME=singko-prjkt
+export TARGET_KERNEL_BUILD_USER=Subreker
+export TARGET_KERNEL_BUILD_HOST=singko-prjkt
+
 # Clone dependencies
 git clone --depth 1 https://github.com/singko-prjkt/vendor_xiaomi_merlin -b 14 vendor/xiaomi/merlin
 
