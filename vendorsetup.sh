6178284c9d9e82dedf86c1a48aae227be754cb73
nullptr03 <ayamgorengmanis87@gmail.com>
2023-11-24 03:10:18 +0800

    merlin: Pick all of the required patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,3 +21,9 @@ patchDir="device/xiaomi/merlin/patches"
 echo "Applying RenderEngineThreaded patches!"
 rm -rf frameworks/native/libs/renderengine/threaded/RenderEngineThreaded.cpp
 cp ${patchDir}/frameworks/native/libs/renderengine/threaded/RenderEngineThreaded.cpp frameworks/native/libs/renderengine/threaded/
+
+# Media: Import codecs/omx changes from t-alps-q0.mp1-V9.122.1
+git -C "frameworks/av" am <<<"$(curl -sL "https://github.com/ArrowOS/android_frameworks_av/commit/1fb1c48309cf01deb9e3f8253cb7fa5961c25595.patch")"
+
+# stagefright: remove HW_TEXTRUE usage from SurfaceMediaSource
+git -C "frameworks/av" am <<<"$(curl -sL "https://github.com/ArrowOS/android_frameworks_av/commit/a727c1f68fd30c8e6a4068db9dc26670d4a78f6c.patch")"
