c7e2df2435c8ce8fde43cdc5c0ef49c33fa1d29c
nullptr03 <ayamgorengmanis87@gmail.com>
2023-11-24 00:54:15 +0800

    merlin: vendorsetup: Update BUILD flags & Remote

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,18 @@
-export KBUILD_BUILD_USER=Subreker
-export KBUILD_BUILD_HOST=singko-prjkt
-export BUILD_USERNAME=Subreker
-export BUILD_HOSTNAME=singko-prjkt
-export TARGET_KERNEL_BUILD_USER=Subreker
-export TARGET_KERNEL_BUILD_HOST=singko-prjkt
+export BUILD_USERNAME=Floryn
+export BUILD_HOSTNAME=SingkoLab
+export KBUILD_BUILD_USER=Floryn
+export KBUILD_BUILD_HOST=SingkoLab
+export TARGET_KERNEL_BUILD_USER=Floryn
+export TARGET_KERNEL_BUILD_HOST=SingkoLab
 
 # Clone dependencies
-git clone --depth 1 https://github.com/singko-prjkt/vendor_xiaomi_merlin -b 14 vendor/xiaomi/merlin
+git clone --depth 1 https://github.com/SingkoLab/vendor_xiaomi_merlin -b 14 vendor/xiaomi/merlin
 
 git clone --depth 1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/mt6768
 
-git clone --depth 1 https://github.com/singko-prjkt/device_mediatek_sepolicy_vndr -b 14 device/mediatek/sepolicy_vndr
+git clone --depth 1 https://github.com/SingkoLab/device_mediatek_sepolicy_vndr -b 14 device/mediatek/sepolicy_vndr
 
-git clone --depth 1 https://github.com/singko-prjkt/hardware_mediatek -b 14 hardware/mediatek
+git clone --depth 1 https://github.com/SingkoLab/hardware_mediatek -b 14 hardware/mediatek
 
 # Configure the patches path
 patchDir="device/xiaomi/merlin/patches"
