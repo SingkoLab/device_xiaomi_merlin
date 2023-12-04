b8e73145bfeac984c599168b3dc8103cd2be7004
Woomymy <woomy@woomy.be>
2023-12-04 18:41:46 +0800

    merlin: Link sensor blobs against `android.hardware.sensors@1.0-conv… …ert`
    * `libndksensorbridge` was converted to AIDL and is no longer linked to
      this static library that contains symbols required by our sensor
      blobs. Add a shim linked to `android.hardware.sensors@1.0-convert` to
      fix our sensors
    
    Change-Id: I969f97bb15d0abcc71f87352722995db3826e122
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -403,7 +403,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libsensorndkbridge_mtk
+    libsensorndkbridge \
+    libshim_sensors
 
 # Shims
 PRODUCT_PACKAGES += \
