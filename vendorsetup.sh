fb61ee7d997c1ef78f4e5fd41caa2e0496fe8ffb
nullptr03 <ayamgorengmanis87@gmail.com>
2024-03-29 14:42:44 +0800

    merlin: Cleanup patches & Update build users

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
-export BUILD_USERNAME=Floryn
+export BUILD_USERNAME=TahuGehot
 export BUILD_HOSTNAME=SingkoLab
-export KBUILD_BUILD_USER=Floryn
+export KBUILD_BUILD_USER=TahuGehot
 export KBUILD_BUILD_HOST=SingkoLab
-export TARGET_KERNEL_BUILD_USER=Floryn
+export TARGET_KERNEL_BUILD_USER=TahuGehot
 export TARGET_KERNEL_BUILD_HOST=SingkoLab
 
 # Clone dependencies
@@ -13,11 +13,3 @@ git clone --depth 1 https://github.com/SingkoLab/KernelTree-MT6768 -b 14 kernel/
 git clone --depth 1 https://github.com/SingkoLab/device_mediatek_sepolicy_vndr -b 14 device/mediatek/sepolicy_vndr
 
 git clone --depth 1 https://github.com/SingkoLab/hardware_mediatek -b 14 hardware/mediatek
-
-# Configure the patches path
-patchDir="device/xiaomi/merlin/patches"
-
-# Patch RenderEngineThreaded
-echo "Applying RenderEngineThreaded patches!"
-rm -rf frameworks/native/libs/renderengine/threaded/RenderEngineThreaded.cpp
-cp ${patchDir}/frameworks/native/libs/renderengine/threaded/RenderEngineThreaded.cpp frameworks/native/libs/renderengine/threaded/
