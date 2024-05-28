215b094ac8d2b1f31ec4384c88262893afe8344d
nuuwy0 <planetgames88@gmail.com>
2024-05-28 15:17:42 +0000

    merlin: vendorsetup: Update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,6 @@
-export BUILD_USERNAME=TahuGehot
+export BUILD_USERNAME=nuuwy0
 export BUILD_HOSTNAME=SingkoLab
-export KBUILD_BUILD_USER=TahuGehot
+export KBUILD_BUILD_USER=nuuwy0
 export KBUILD_BUILD_HOST=SingkoLab
-export TARGET_KERNEL_BUILD_USER=TahuGehot
+export TARGET_KERNEL_BUILD_USER=nuuwy0
 export TARGET_KERNEL_BUILD_HOST=SingkoLab
-
-# Clone dependencies
-git clone --depth 1 https://github.com/SingkoLab/vendor_xiaomi_merlin -b 14 vendor/xiaomi/merlin
-
-git clone --depth 1 https://github.com/SingkoLab/KernelTree-MT6768 -b 14 kernel/xiaomi/mt6768
-
-git clone --depth 1 https://github.com/SingkoLab/device_mediatek_sepolicy_vndr -b 14 device/mediatek/sepolicy_vndr
-
-git clone --depth 1 https://github.com/SingkoLab/hardware_mediatek -b 14 hardware/mediatek
-
-git clone --depth 1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
